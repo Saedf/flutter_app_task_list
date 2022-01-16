7a33b6d5bf1ac2f0b6d509c468c231e4719c8674
saed.fathi@gmial.com <saed.fathi@gmail.com>
2022-01-16 13:20:50 +0330

    Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_task_list/data.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+const taskBoxName = 'tasks';
 Future<void> main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(PriorityAdapter());
+
+  await Hive.openBox<Task>(taskBoxName);
   runApp(const MyApp());
 }
 
@@ -25,6 +31,87 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final box = Hive.box<Task>(taskBoxName);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To Do List'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => EditTaskScreen(),
+              ),
+            );
+          },
+          label: Text('Add New Task')),
+      body: ValueListenableBuilder<Box<Task>>(
+        valueListenable: box.listenable(),
+        builder: (context, box, child) {
+          return ListView.builder(
+            itemCount: box.values.length,
+            itemBuilder: (context, index) {
+              final Task task = box.values.toList()[index];
+              return Container(
+                child: Text(
+                  task.name,
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class EditTaskScreen extends StatelessWidget {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Task'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final task = Task();
+            task.name = _controller.text;
+            task.priority = Priority.low;
+            if (task.isInBox) {
+              task.save();
+            } else {
+              final Box<Task> box = Hive.box(taskBoxName);
+              box.add(task);
+            }
+
+            Navigator.pop(context);
+          },
+          label: Text('Save Changes')),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              label: Text('Add a task for today...'),
+            ),
+          )
+        ],
       ),
     );
   }
