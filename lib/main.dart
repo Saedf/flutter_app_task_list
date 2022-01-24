b6b51c117e5d3550f8b80f47750f15839c8eb9a5
saed.fathi@gmial.com <saed.fathi@gmail.com>
2022-01-24 14:46:05 +0330

    till 19 minuts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_task_list/data.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,8 +11,14 @@ void main() async {
   Hive.registerAdapter(TaskAdapter());
   Hive.registerAdapter(PriorityAdapter());
   await Hive.openBox<Task>(taskBoxName);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: primaryVariantColor),
+  );
   runApp(const MyApp());
 }
+
+const Color primaryColor = Color(0xff794cff);
+const Color primaryVariantColor = Color(0xff5c0aff);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,6 +26,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff1D2830);
+    final secondaryColor = Color(0xffAFBED0);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -30,7 +40,16 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.light(
+          primary: primaryColor,
+          primaryVariant: primaryVariantColor,
+          background: Color(0xfff3f5f8),
+          onSurface: primaryTextColor,
+          onBackground: primaryTextColor,
+          onPrimary: Colors.white,
+          secondary: primaryColor,
+          onSecondary: Colors.white,
+        ),
       ),
       home: const HomeScree(),
     );
@@ -43,10 +62,8 @@ class HomeScree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final box = Hive.box<Task>(taskBoxName);
+    final ThemeData = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('To Do List'),
-      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
@@ -55,24 +72,87 @@ class HomeScree extends StatelessWidget {
             ));
           },
           label: Text('Add New Task')),
-      body: ValueListenableBuilder<Box<Task>>(
-        valueListenable: box.listenable(),
-        builder: (context, box, child) {
-          return ListView.builder(
-              itemCount: box.values.length,
-              itemBuilder: (context, index) {
-                final Task task = box.values.toList()[index];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    child: Text(
-                      task.name,
-                      style: TextStyle(fontSize: 15),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 102,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    ThemeData.colorScheme.primary,
+                    ThemeData.colorScheme.primaryVariant
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'To Do List',
+                          style: ThemeData.textTheme.headline6!
+                              .apply(color: ThemeData.colorScheme.onPrimary),
+                        ),
+                        Icon(
+                          CupertinoIcons.share,
+                          color: ThemeData.colorScheme.onPrimary,
+                        ),
+                      ],
                     ),
-                  ),
-                );
-              });
-        },
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      height: 38,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(19),
+                        color: ThemeData.colorScheme.onPrimary,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 20,
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(CupertinoIcons.search),
+                          label: Text('Search Tasks'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ValueListenableBuilder<Box<Task>>(
+                valueListenable: box.listenable(),
+                builder: (context, box, child) {
+                  return ListView.builder(
+                      itemCount: box.values.length,
+                      itemBuilder: (context, index) {
+                        final Task task = box.values.toList()[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Text(
+                              task.name,
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        );
+                      });
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
